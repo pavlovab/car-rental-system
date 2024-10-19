158250cf4736bcf6bf417c74c057ac5512b86465
Borislava <pavlova-b@outlook.com>
2024-10-19 18:18:00 +0300

    Configure and initialize SQLAlchemy Database

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
+from config import Config
+from models import db
+
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+# Initialize SQLAlchemy Database
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    # Configure Database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///car_rental.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
